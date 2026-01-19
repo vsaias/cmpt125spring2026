c07ede28733424f749dd289f94bdce5e05b723c8
Toby Donaldson <tjd@sfu.ca>
2026-01-19 13:28:04 -0800

    updated week 3 notes

--- notes/week3/pointers.cpp
+++ notes/week3/pointers.cpp
@@ -11,14 +11,14 @@ int main()
     cout << "int(c): " << int(c) << "\n";    // 84
     cout << "    &c: " << (void*)&c << "\n"; // 0x7ffee572412a (or whatever the address is)
 
-    char* p = &c; // p is a char pointer that points to c
-    cout << "\n";
-    cout << "    *p: " << *p << "\n"; // T
+    // char* p = &c; // p is a char pointer that points to c
+    // cout << "\n";
+    // cout << "    *p: " << *p << "\n"; // T
 
-    cout << "\n";
-    *p = 'a';
-    cout << "*p = 'a';\n";
-    cout << "\n";
-    cout << "     c: " << c << "\n"; // a
-    cout << "    *p: " << *p << "\n"; // a
+    // cout << "\n";
+    // *p = 'a';
+    // cout << "*p = 'a';\n";
+    // cout << "\n";
+    // cout << "     c: " << c << "\n"; // a
+    // cout << "    *p: " << *p << "\n"; // a
 }
